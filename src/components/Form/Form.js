3efe96ad4d1b7c94887a4bb5c5500b5c77e3933a
navneek-singh10 <navdoad01@gmail.com>
2023-02-11 13:14:28 +0530

    edited

---
+++ src/components/Form/Form.js
@@ -0,0 +1,121 @@
+import React, { useState } from 'react';
+import "./Form.css";
+
+function FormPage() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phoneNumber: '',
+    address: ''
+  });
+  
+  const [memberList, setMemberList] = useState([]);
+
+  const handleChange = event => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setMemberList([...memberList, formData]);
+    setFormData({
+      name: '',
+      email: '',
+      phoneNumber: '',
+      address: ''
+    });
+  };
+
+  return (
+    <div>
+      <h1>Fitness Club Registration</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Email:
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Phone Number:
+            <input
+              type="tel"
+              name="phoneNumber"
+              value={formData.phoneNumber}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Address:
+            <input
+              type="text"
+              name="address"
+              value={formData.address}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <button type="submit">Add Member</button>
+      </form>
+      <button>
+        <a href="/member-list">View Member List</a>
+      </button>
+      {memberList.length > 0 && (
+        <MemberListPage memberList={memberList} />
+      )}
+    </div>
+  );
+}
+function MemberListPage({ memberList }) {
+  return (
+    <div>
+      <h1>Member List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone Number</th>
+            <th>Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          {memberList.map(member => (
+            <tr key={member.email}>
+              <td>{member.name}</td>
+              <td>{member.email}</td>
+              <td>{member.phoneNumber}</td>
+              <td>{member.address}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+
+
+export default FormPage;
